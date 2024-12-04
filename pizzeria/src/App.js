c7ed0145b5aad9a85f127291f499a1e9ba325f3c
Aldrit <aldritkuleta2@gmail.com>
2024-12-04 18:04:45 +0100

    ajout de la popup style et structure sans la logqique

--- pizzeria/src/App.js
+++ pizzeria/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header.js';
 import Container from './components/Container/container.js';
+import Popup from './components/Popup/Popup';
 import Footer from './components/Footer/Footer.js';
 function App() {
   return (
     <div className="App">
         <Header/>
         <Container/>
+        <Popup/>
         <Footer/>
     </div>
   ); 
